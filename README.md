01c6675a6aea66d38c6a1504d746963cde74896c
Tim <timinth@gmail.com>
2015-02-19 11:39:46 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Add to this later
---
+++ Ribbit/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Ribbit
+//
+//  Created by Tim Walsh on 2/19/15.
+//  Copyright (c) 2015 ClassicTim. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
